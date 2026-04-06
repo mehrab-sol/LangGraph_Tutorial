dd83e6e8fb50e1bdd282afe4df85858049fd10cb
mehrab-sol <fmchowdhury@siliconorchard.com>
2026-04-06 16:15:06 +0600

    Langgraph will LLM

--- Ex2_two_nodes_llm.py
+++ Ex2_two_nodes_llm.py
@@ -1,24 +1,68 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-from langgraph.graph import StateGraph
-from state import AgentState
+import os
+from dotenv import load_dotenv
 
-llm = ChatOpenAI(model = "gpt-4o")
+# api key config
+load_dotenv()
+api_key=os.getenv("OPENROUTER_API_KEY").strip()
 
-# node-1: calling the LLM
-def call_llm(state: AgentState) -> dict:
-    response = llm.invoke(state["messages"])
-    return {"messages": [response]} # Only will return what's been changed
-
-# node-2: validate the response
-def validate_response(state: AgentState) -> dict:
-    last_msg = state["messages"][-1].content
-    if "error" in last_msg.lower():
-        return {"current_step": "retry", "retry_count": state["retry_count"]+1}
-    return {"current_step": "Done", "final_answer": last_msg}
+if not api_key:
+    raise ValueError("API key not found in the directory or the key is not valid Or expired!")
 
 
-# wiring nodes into graph
-graph = StateGraph(AgentState)
-graph.add_node("call_llm", call_llm)
-graph.add_node("validate", validate_response)
+# 1 - connect the LLM
+
+llm = ChatOpenAI(
+    model = 'minimax/minimax-m2.5:free',
+    base_url ="https://openrouter.ai/api/v1",
+    api_key = api_key,
+    temperature = 0.7,
+)
+
+
+# 1 - State
+class State(TypedDict):
+    topic: str
+    prompt: str  # node-1
+    answer: str  # node-2
+
+
+
+# 2 - node_1 [prompt formating]
+def build_prompt_node(state: State) -> dict:
+    print(f"[Node-1] -> Prompt for topic: '{state['topic']}'")
+    prompt = f"Explain '{state['topic']}' in exactly 1 sentence. Be concise."
+    return {"prompt": prompt}  # Only update the prompt
+
+
+# 3 - node_2 [Call the LLM with the prompt from state]
+def call_llm_node(state: State) -> dict:
+    print(f"[Node-2] -> Calling the LLM...")
+    response = llm.invoke([HumanMessage(content = state["prompt"])])
+    return {"answer": response.content}
+
+
+# 4 - graph structure
+graph = StateGraph(State)
+
+graph.add_node("build_prompt", build_prompt_node)
+graph.add_node("call_llm", call_llm_node)
+
+graph.add_edge(START, "build_prompt")
+graph.add_edge("build_prompt", "call_llm") # node_1 -> node_2
+graph.add_edge("call_llm", END)
+
+app = graph.compile()
+
+
+if __name__ == "__main__":
+    topics = ["Langgraph", "QLoRA fine-tuning", "what a nural network is"]
+
+    for topic in topics:
+        print(f"\n{'='*50}")
+        print(f"Topic: {topic}")
+        result = app.invoke({"topic": topic, "prompt": "", "answer": ""})
+        print(f"Answer: {result['answer']}")
